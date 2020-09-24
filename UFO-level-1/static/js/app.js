6f3ae9afb722a736fe9a476f799779385acb12c8
Brooke Fish <brookecrofts1@gmail.com>
2020-09-24 09:30:09 -0600

    Final working code Thurs

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,7 +11,6 @@ console.log(data);
 //Must buildtable first  - Activity 14-3-3 + bonus steps for reference
 // Step 1: Loop Through `data` and console.log each alien report object
 // Step 2:  Use d3 to append one table row `tr` for each alien report object
-// Don't worry about adding cells or text yet, just try appending the `tr` elements.
 // Step 3:  Use `Object.entries` to console.log each alien report value
 // Step 4: Use d3 to append 1 cell per alien report value (date,city,st,country, shape, duration)
 function buildtable(data) {
